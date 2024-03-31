235d46b9fb3e3deaef8b94fe39342df6321caef3
adrianvg71 <adrianvg71@educastur.es>
2024-03-31 19:13:33 +0200

    server.js

---
+++ server.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+
+// Rutas
+const adminRouter = require("./public/routes/adminRoutes");
+const userRouter = require("./public/routes/userRoutes");
+const loginRouter = require("./public/routes/loginRoutes");
+const insertRouter = require("./public/routes/insertRoutes")
+const homeRouter = require("./public/routes/homeRoutes")
+const productosRouter = require("./public/routes/productosRoutes")
+const carritoRouter = require("./public/routes/carritoRoutes")
+const comprarRoutes = require("./public/routes/comprarRoutes")
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.static(__dirname + '/public'))
+
+var isUser = function(req, res, next) {
+  if(req.session && req.session.rol != undefined) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+}
+
+var isAdmin = function(req, res, next) {
+  if(req.session && req.session.rol === "ADMIN") {
+    return next();
+  } else {
+    return res.send("No tienes acceso a esto")
+  }
+}
+
+app.use("/login", loginRouter)
+
+
+app.get('/', (req, res) => {
+  res.redirect("/home")
+}) 
+
+app.use('/admin', isAdmin, adminRouter)
+
+app.use('/user', userRouter)
+
+app.post('/logout', function(req, res) {
+  req.session.destroy();
+  res.redirect("/")
+})
+
+app.get("/getUser", function(req, res){
+  res.json({ username: req.session.username })
+}) 
+
+app.use("/home", homeRouter)
+
+app.use('/insertar', insertRouter)
+
+app.use('/productos', productosRouter)
+
+app.use('/carrito', carritoRouter)
+
+app.use('/comprar', comprarRoutes)
+
+app.get("/prueba", function(req,res) {
+  res.render("prueba2", {titulo: "hola"})
+})
+
+app.post("/test", function(req,res) {
+  var variable = req.body.variable;
+
+  res.json({ title: variable, variable: variable })
+})
+
+const server = app.listen(8080, () => {
+    console.log('Servidor web iniciado en http://localhost:8080')
+})
